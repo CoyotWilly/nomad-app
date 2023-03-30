a32d5e8bb2372aa1de14c4490208b6fa3f53bec1
CoyotWilly <coyotwilly@gmail.com>
2023-03-30 15:36:47 +0200

    Permission request for camera added

--- app/src/main/java/com/coyotwilly/nomad/FutureTripCreate.kt
+++ app/src/main/java/com/coyotwilly/nomad/FutureTripCreate.kt
@@ -1,37 +1,81 @@
 package com.coyotwilly.nomad
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.result.ActivityResult
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import java.io.File
 
 class FutureTripCreate : AppCompatActivity() {
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
+    private val CAMERA_PERMISSION_CODE = 1
+    private lateinit var imgUri: Uri
     private val galleryImg = registerForActivityResult(ActivityResultContracts.GetContent(),
         ActivityResultCallback {
             findViewById<ImageView>(R.id.background_preview_future).setImageURI(it)
         })
-    private val cameraContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result: ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val bitmap = result.data?.extras?.get("data") as Bitmap
-            findViewById<ImageView>(R.id.background_preview_future).setImageBitmap(bitmap)
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_future_trip_create)
+        imgUri = createUri()
+        registerPictureLauncher()
 
         findViewById<ImageView>(R.id.background_preview_future).setOnClickListener {
             galleryImg.launch("image/*")
         }
         findViewById<Button>(R.id.gallery_bnt).setOnClickListener {
-            cameraContract.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+            checkCameraPermissionAndOpenCamera()
+        }
+    }
+    private fun createUri() : Uri {
+        val imgFile = File(applicationContext.filesDir, "camera_photo.jpg")
+        return FileProvider.getUriForFile(
+            applicationContext,
+            "com.coyotwilly.nomad.fileProvider",
+            imgFile
+        )
+    }
+    private fun registerPictureLauncher() {
+        takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture(),
+            ActivityResultCallback {result ->
+                try {
+                    if (result) {
+                        findViewById<ImageView>(R.id.background_preview_future).setImageURI(null)
+                        findViewById<ImageView>(R.id.background_preview_future).setImageURI(imgUri)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+        })
+    }
+    private fun checkCameraPermissionAndOpenCamera() {
+        if (ActivityCompat.checkSelfPermission(this ,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        } else {
+            takePictureLauncher.launch(imgUri)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if((grantResults.isNotEmpty()) and (grantResults.get(0) == PackageManager.PERMISSION_GRANTED)) {
+                takePictureLauncher.launch(imgUri)
+            } else {
+                Toast.makeText(this, "Camera permissions denied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
